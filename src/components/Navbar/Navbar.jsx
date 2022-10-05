d145c1477937e8bcaade3d6cf46937f9178f3679
Valentin Paul <valentin@vamptech.de>
2022-10-05 22:27:13 +0200

    Navigation bar with links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,8 +7,8 @@ import * as CONSTS from "../../utils/consts";
 const Navbar = (props) => {
   return (
     <nav>
-      <Link to={PATHS.HOMEPAGE} className="nav__projectName">
-        {CONSTS.CAPITALIZED_APP} - created with IronLauncher
+      <Link to={PATHS.HOMEPAGE} className="navLink Logo">
+        Schluckreflex
       </Link>
 
       <div className="nav__authLinks">
@@ -23,10 +23,16 @@ const Navbar = (props) => {
           </>
         ) : (
           <>
-            <Link to={PATHS.SIGNUPPAGE} className="authLink">
+          <Link to="/recipes" className="navLink">
+          Recipes
+          </Link>
+          <Link to="/contact" className="navLink">
+            Kontakt
+          </Link>
+            <Link to={PATHS.SIGNUPPAGE} className="navLink">
               Signup
             </Link>
-            <Link to={PATHS.LOGINPAGE} className="authLink">
+            <Link to={PATHS.LOGINPAGE} className="navLink">
               Log In
             </Link>
           </>
