0df1c7fcc84e383d9c2d5ee265eeea096c608a38
Valentin Paul <valentin@vamptech.de>
2022-10-11 18:17:43 +0200

    created recipeDetails.js page and implemented functionability

---
+++ src/pages/RecipeDetails.js
@@ -0,0 +1,80 @@
+import axios, { Axios } from "axios"
+import { useParams } from "react-router-dom"
+import { useState, useEffect } from "react";
+import "./css/RecipeDetails.css"
+
+function RecipeDetails(){
+
+    const {recipeId} = useParams();
+    const [recipeDetails, setRecipeDetails] = useState(null)
+    console.log(recipeId)
+
+    useEffect(()=>{
+        getRecipeDetails()
+    }, [recipeId])
+
+    const getRecipeDetails = ()=>{
+        axios
+        .get(`${process.env.REACT_APP_SERVER_URL}/recipes/${recipeId}`)
+        .then((response)=> setRecipeDetails(response.data))
+        .catch((e)=>{console.log("error getting recipe details --->", e)})
+    }
+
+    
+    return(
+        <div className="is-family-sans-serif container">
+        {recipeDetails === null? null 
+        : (
+            <div>
+            <h1 className=" is-size-3-touch is-size-3-desktop has-text-centered is-uppercase pt-2">{recipeDetails.recipeName}</h1>
+            <section className="section is-medium section-ingredientes">
+            <div className="box box-ingredientes">
+            <h4 className="is-size-4-touch is-size-3-desktop zutaten">Zutaten:</h4>
+            <hr className="hr"/>
+            <div className="ingrediente-details">
+             <h3 className="is-size-5-touch is-size-3-desktop">{recipeDetails.ingredientes.map((items,index)=>{    
+                
+                return(
+            <div className="">
+                {items.map((subItems, sIndex)=>{
+                    return(
+                        <div className="ingrediente-line ingrediente-line-left"> 
+                        <h5 className="mr-3 is-size-5-touch is-size-6-desktop">{subItems.amount}</h5>
+                        
+                        </div>
+                    )
+                })}
+            </div>
+        )}
+        )}
+               </h3>
+               <h3 className="is-5">{recipeDetails.ingredientes.map((items,index)=>{    
+                
+                return(
+            <div className="">
+                {items.map((subItems, sIndex)=>{
+                    return(
+                        <div className="ingrediente-line"> 
+                       
+                        <h5 className="is-size-5-touch is-size-6-desktop">{subItems.ingrediente}</h5>
+                        </div>
+                    )
+                })}
+            </div>
+        )}
+        )}
+               </h3>
+            </div>
+            </div>
+            </section>
+            
+            <p>{recipeDetails.description}</p>
+            </div>
+        )}
+        </div>
+        
+    
+    )
+}
+
+export default RecipeDetails
