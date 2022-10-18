aa2f2e46881fb85ec82cf5b79ba928129645bbd6
Valentin Paul <valentin@vamptech.de>
2022-10-18 19:04:17 +0200

    made homepage with moving header, background image und description

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,73 @@
-import logo from "../logo.svg";
+
 import "../App.css";
+import "./css/Homepage.css"
+import MovingText from "react-moving-text"
+import { useEffect, useState } from "react";
+import background from "../images/herbs-homepage.jpg"
+
 
 function HomePage() {
+
+  useEffect(()=>{
+    handleAnimationName()
+  },[])
+
+  const [animationSchluck, setAnimationSchluck] = useState("")
+  const [animationReflex, setAnimationReflex] = useState("")
+
+  const handleAnimationName = () => {
+    setTimeout(()=>{
+      setAnimationSchluck(["Schluck"])
+    }, 800)
+    setTimeout(()=>{
+      setAnimationReflex("reflex")
+    }, 1400)
+    
+
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="animation-header" style={{ backgroundImage: `url(${background})` }}>
+      <div className="schluckreflex">
+      <div className="schluck ">
+      <MovingText
+      type="slideInFromBottom"
+      duration="600ms"
+      delay="0.8s"
+      direction="normal"
+      timing="ease"
+      iteration="1"
+      fillMode="none">
+      {animationSchluck}
+      </MovingText>
+      </div>
+      <div className="reflex ">
+      <MovingText
+      type="slideInFromBottom"
+      duration="900ms"
+      delay="1.4s"
+      direction="normal"
+      timing="ease-in"
+      iteration="1"
+      fillMode="none">
+     {animationReflex}
+    </MovingText>
+      </div>
+      </div>
+      
+      <p className="headline">Feinste Rezepte die dich schlucken lassen</p>
+
+      <button className="button is-primary is-light rezepte-button center">Zu den Rezepten</button>
+      </div>
+
+      <p className="homepage-description">Schluckreflex ist ein Food-Kollektiv aus Graz & Wien, dass euch in die Welt unseres Essens-Ateliers mitnimmt. Unser Ziel ist es, unsere kullinarischen Kreationen in die Welt zu tragen und dadurch euren Alltag zu versüßen. </p>
+      <div className="align-center">
+      <button className=" button is-primary button-überuns">Über uns</button>
+      </div>
+      
+
+        
       </header>
     </div>
   );
