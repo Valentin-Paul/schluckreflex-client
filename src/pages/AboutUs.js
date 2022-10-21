8718f9c8c61f953755644ce08801438b9324d637
Valentin Paul <valentin@vamptech.de>
2022-10-21 12:36:03 +0200

    created content for AboutUs.js

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,22 @@
+import "./css/aboutus.css"
+import chefCooking from "../images/chef-cooking.jpg"
+import chefGarnishing from "../images/chef-garnishing.jpg"
+
+
+function AboutUs(){
+
+
+    return(
+        <div className="about-us">
+        <div className="about-us-img" style={{ backgroundImage: `url(${chefCooking})` }}>
+        <h1>Über Uns</h1>
+        </div>
+        
+       <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+       <img className="about-us-second-img" src={chefGarnishing}></img>
+       <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et</p>
+        </div>
+    )
+}
+
+export default AboutUs
