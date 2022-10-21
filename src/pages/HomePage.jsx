9ed1b216b55340d15d26d0d82d598d1fe289c865
Valentin Paul <valentin@vamptech.de>
2022-10-21 10:36:01 +0200

    implemented scrolling features on Homepage.js

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,9 +4,12 @@ import "./css/Homepage.css"
 import MovingText from "react-moving-text"
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { Animator, ScrollContainer, ScrollPage, batch, Fade, FadeIn, FadeOut, Move, MoveIn, MoveOut, Sticky, StickyIn, StickyOut, Zoom, ZoomIn, ZoomOut } from "react-scroll-motion";
+
 import * as PATHS from "../utils/paths";
 import background from "../images/herbs-homepage.jpg"
 import walnut from "../images/walnut.jpeg"
+import arrow from "../images/up-arrow.png"
 
 
 function HomePage() {
@@ -15,76 +18,125 @@ function HomePage() {
     handleAnimationName()
   },[])
 
+
+
   const [animationSchluck, setAnimationSchluck] = useState("")
   const [animationReflex, setAnimationReflex] = useState("")
+  const FadeUp = batch( MoveIn(),FadeIn());
 
   const handleAnimationName = () => {
     setTimeout(()=>{
       setAnimationSchluck(["Schluck"])
-    }, 800)
+    }, 500)
     setTimeout(()=>{
       setAnimationReflex("reflex")
-    }, 1400)
+    }, 1200)
     
+   
 
   }
   return (
-    <div className="App">
-      <header className="App-header">
-      <div className="animation-header" style={{ backgroundImage: `url(${background})` }}>
-      <div className="schluckreflex">
-      <div className="schluck ">
-      <MovingText
-      type="slideInFromBottom"
-      duration="600ms"
-      delay="0.8s"
-      direction="normal"
-      timing="ease"
-      iteration="1"
-      fillMode="none">
-      {animationSchluck}
-      </MovingText>
-      </div>
-      <div className="reflex ">
-      <MovingText
-      type="slideInFromBottom"
-      duration="900ms"
-      delay="1.4s"
-      direction="normal"
-      timing="ease-in"
-      iteration="1"
-      fillMode="none">
-     {animationReflex}
+<div className="App">
+    <ScrollContainer>
+    <ScrollPage> 
+    <a name="top"></a>
+    <div className="animation-header" style={{ backgroundImage: `url(${background})` }}>
+    
+    <div className="schluckreflex future-cop">
+    <div className="schluck cop">
+    <MovingText
+    type="slideInFromBottom"
+    duration="600ms"
+    delay="0.5s"
+    direction="normal"
+    timing="ease"
+    iteration="1"
+    fillMode="none">
+    {animationSchluck}
     </MovingText>
-      </div>
-      </div>
-      
-      <p className="headline">Feinste Rezepte die dich schlucken lassen</p>
-      <Link
-            to={PATHS.RECIPES}
-            className="button is-primary is-light rezepte-button center"
-            width="100%"
-          >
-            Zu den Rezepten
-          </Link>
-      </div>
-
-   
-    <div className="section-description">
+    </div>
+    <div className="reflex future">
+    <MovingText
+    type="slideInFromBottom"
+    duration="900ms"
+    delay="1.2s"
+    direction="normal"
+    timing="ease-in"
+    iteration="1"
+    fillMode="none">
+   {animationReflex}
+  </MovingText>
+    </div>
+    </div>
+    
+    <p className="headline">Unsere feinsten Rezepte für dich</p>
+    <Link
+          to={PATHS.RECIPES}
+          className="button is-primary is-light rezepte-button center"
+          width="100%"
+        >
+          Zu den Rezepten
+        </Link>
+    </div>
+     </ScrollPage>
+    
+    <ScrollPage>
+    <Animator animation={FadeUp}>
+      <span> 
+      <div className="section-description">
+      <h3 className="über-uns-header">Über uns</h3>
     <p className="homepage-description">Schluckreflex ist ein Food-Kollektiv aus Graz & Wien, dass euch in die Welt unseres Essens-Ateliers mitnimmt. Unser Ziel ist es, unsere kullinarischen Kreationen in die Welt zu tragen und dadurch euren Alltag zu versüßen. </p>
       <div className="align-center">
-      <button className=" button is-primary button-überuns">Über uns</button>
+      <button className=" button is-primary button-überuns">mehr</button>
       </div>
     </div>
+      </span>
+      
+    </Animator>
+    </ScrollPage>
+     <ScrollPage>
+     <Animator animation={Fade()}>
+   
+     <div className="contact-homepage" style={{ backgroundImage: `url(${walnut})` }}>
+     <h3 className="kontakt-header">Kontakt</h3>
+      <p className="homepage-description homepage-contact">Wir freuen und über Fragen, Anmerkungen, Kritik, Rezeptwünsche oder eigens kreiirte Rezepte die wir ausprobieren sollen oder einfach eine nette Nachricht.</p>
+      <div className="align-center">
+      <button className=" button is-primary is-light button-contact">Nachricht</button>
+      </div>
+      </div>
+  
+      </Animator>
+     
+     </ScrollPage>
+     <Animator animation={MoveOut()}>
+  
+ 
+  </Animator>
+   
+  <a href="#top">
+  <div className="to-top">
+  <img src={arrow}></img>
+  </div>
+ 
+  </a>
+  
+
+
+
+
+    </ScrollContainer>
+
+
+    
+      <header className="App-header">
+     
+
+   
+ 
     
 
   
-      <div className="contact-homepage" style={{ backgroundImage: `url(${walnut})` }}>
-      <p className="homepage-description">Wir freuen und über Fragen, Anmerkungen, Kritik, Rezeptwünsche oder eigens kreiirte Rezepte die wir ausprobieren sollen oder einfach eine nette Nachricht.</p>
-      <div className="align-center">
-      <button className=" button is-primary is-light button-contact">Kontakt</button>
-      </div>
-      </div>
+    
       
 
         
