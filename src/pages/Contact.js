8af8aa3deb9de72b774730eef04d861dcc53caf6
Valentin Paul <valentin@vamptech.de>
2022-10-23 18:33:02 +0200

    implemented content and style od contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,26 @@
 import "./css/contact.css"
+import contactImage from "../images/contactImage.jpg"
+import insta from "../images/instagram.png"
+import mail from "../images/email.png"
 
-function AboutUs (){
+function Contact (){
 
     return(
-        <div>
+        <div style={{ backgroundImage: `url(${contactImage})` }} className="contact-background">
+        <h1>Kontakt</h1>
+        <p>Wenn du mehr über Schluckreflex, das Team oder Rezepte hast, bzw. du uns Fragen, Anmerkungen, Kritik, Rezeptwünsche oder Fanpost schicken möchtest, kontaktiere uns einfach über Instagram oder Mail. Wir freuen uns über Eure Nachrichten.</p>
+        <div className="contact-icons">
+        <a href="https://www.instagram.com/schluck_reflex/">
+            <img className="icon" src={insta}></img>
+        </a>
 
+        <a href= "mailto:abc@example.com">
+        <img className="icon" src={mail}></img>
+        </a>
+        </div>
+    
         </div>
     )
 }
 
-export default AboutUs
+export default Contact
