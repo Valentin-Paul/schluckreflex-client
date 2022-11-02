0c5b04c9d0bb9fdc47d2b99307d8189908a2cab4
Valentin Paul <valentin@vamptech.de>
2022-11-02 21:35:14 +0100

    added date und tag to recipeDetails page

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -23,6 +23,28 @@ function RecipeDetails(){
         .catch((e)=>{console.log("error getting recipe details --->", e)})
     }
 
+    const getDate = (element) => {
+        let months = [
+          "Januar",
+          "Februar",
+          "März",
+          "April",
+          "Mai",
+          "Juni",
+          "August",
+          "September",
+          "Oktober",
+          "November",
+          "Dezember",
+        ];
+        return (
+          <p>
+            {months[~~element.updatedAt.slice(5, 7) - 2]}{" "}
+            {element.updatedAt.slice(0, 4)}
+          </p>
+        );
+      };
+
     
     return(
         <div className="is-family-sans-serif container">
@@ -30,6 +52,18 @@ function RecipeDetails(){
         : (
             <div>
             <h1 className="section is-size-3-touch is-size-3-desktop has-text-centered is-uppercase pt-3">{recipeDetails.recipeName}</h1>
+            <div className="align-center">
+            <img className="recipe-image" src={recipeDetails.imageUrl}></img>
+            </div>
+            <div className="align-infos">
+            <div className="minor-infos">
+            {getDate(recipeDetails)} 
+            <p>Küche: {recipeDetails.tags[0]}</p>
+            </div>
+            </div>
+         
+            
+           
             <section className="is-medium section-ingredientes">
             <div className="box box-ingredientes">
             <h4 className="is-size-4-touch is-size-3-desktop zutaten">Zutaten:</h4>
